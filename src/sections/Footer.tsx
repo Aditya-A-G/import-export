db174ae6b1fec2a08917413b2e3beab2615eb1e5
Aditya Ghadge <adityaghadge360@gmail.com>
2024-08-26 17:58:37 +0530

    update footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -5,17 +5,21 @@ import linkedinLogo from "/public/linkedin.png";
 
 export const Footer = () => {
   return (
-    <footer className="mt-16">
-      <div className=" flex flex-col md:flex-row w-full justify-between items-center md:px-16  ">
+    <footer className="mt-10 border-t border-gray-300 py-5">
+      <div className=" flex flex-col md:flex-row w-full justify-between items-center md:px-16   ">
         <div className="w-2/3 md:w-1/3 text-center md:text-start  md:text-lg">
-        Waterfront Market Wholesale, Deira Dubai
+          <div className="font-bold">Address :</div>
+          Waterfront Market Wholesale, Deira Dubai
         </div>
 
         <div className="w-11/12 md:w-1/3 grid place-items-center md:place-items-start mt-6 gap-6">
-          <div className="flex flex-col">
+          <div className=" text-center flex flex-col md:text-start">
             <a className="underline">+971 52 2148 918</a>
-            <a href="mailto:aarfoodstufftrading@gmail.com" className="underline">
-            aarfoodstufftrading@gmail.com
+            <a
+              href="mailto:aarfoodstufftrading@gmail.com"
+              className="underline"
+            >
+              aarfoodstufftrading@gmail.com
             </a>
           </div>
           <div className="flex flex-col justify-center items-center md:items-start gap-5">
@@ -39,9 +43,9 @@ export const Footer = () => {
       </div>
 
       <div className="p-5 mt-10 flex flex-col md:flex-row justify-between items-center gap-3 text-gray-600">
-        <span>Greenhouse Foodstuff Trading LLC</span>
-        <span>Culinary innovation by your side.</span>
-        <span>Copyright 2018</span>
+        <span>Abdul Aziz Rahim Food Stuff Trading LLC</span>
+        <span>Your Trusted Partner in Freshness</span>
+        <span>Copyright 2024</span>
       </div>
     </footer>
   );
