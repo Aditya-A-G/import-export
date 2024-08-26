007c8e2a58d9f7bb8f40d3e26d418dba887a7d24
Aditya Ghadge <adityaghadge360@gmail.com>
2024-08-26 18:24:57 +0530

    update width of the catalogue section

--- src/sections/Catalogue.tsx
+++ src/sections/Catalogue.tsx
@@ -1,12 +1,12 @@
 export const Catalogue = () => {
   return (
     <section className="bg-catalogue w-full bg-no-repeat bg-cover h-[500px] flex flex-col items-center px-5 py-8 md:py-16 lg:py-20 gap-10">
-      <div className="w-full border-b-8 border-white py-5 flex justify-center items-center md:w-1/2">
+      <div className="w-full lg:w-2/5 border-b-8 border-white py-5 flex justify-center items-center md:w-1/2">
         <h3 className="text-white text-3xl lg:text-4xl  font-extrabold text-center">
           Browse Our Catalogue
         </h3>
       </div>
-      <div className="w-full md:w-1/3 lg:w-1/3">
+      <div className="w-full md:w-1/3 lg:w-1/4">
         <div>
           <button className="bg-black text-white w-full h-24 md:h-20 hover:bg-[#4DAEC2]">
             <a href="/AbdulAziz_Catalogue.pdf">UAE</a>
