c8bc30625d60c2fe3900cc988660dd92a4c433a2
Aditya Ghadge <adityaghadge360@gmail.com>
2024-08-26 18:07:59 +0530

    redirect user to mail (call to action)

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -6,8 +6,12 @@ export const CallToAction = () => {
         <h2>Get in touch with us now.</h2>
       </div>
       <div className="w-2/3 md:w-7/12 lg:w-2/5 flex flex-col md:flex-row gap-7 mt-10">
-        <button className="w-full bg-black text-white py-9 md:py-8  lg:py-9 font-bold hover:bg-[#4DAEC2]">BECOME A SUPPLIER</button>
-        <button className="w-full bg-black text-white py-9 md:py-8 lg:py-9 font-bold hover:bg-[#4DAEC2]">BECOME A CUSTOMER</button>
+        <button className="w-full bg-black text-white py-9 md:py-8  lg:py-9 font-bold hover:bg-[#4DAEC2]">
+          <a href="mailto:aarfoodstufftrading@gmail.com">BECOME A SUPPLIER</a>
+        </button>
+        <button className="w-full bg-black text-white py-9 md:py-8 lg:py-9 font-bold hover:bg-[#4DAEC2]">
+          <a href="mailto:aarfoodstufftrading@gmail.com">BECOME A CUSTOMER</a>
+        </button>
       </div>
     </section>
   );
