c6ad844b78f08785e745a8d0c31ec141463815a1
Aditya Ghadge <adityaghadge360@gmail.com>
2024-08-26 12:55:43 +0530

    remove unwanted sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import { Header } from "@/sections/Header";
 import { Carousel } from "@/sections/Carousel";
 import { Gallery } from "@/sections/Gallery";
-import { CompanyPillars } from "@/sections/CompanyPillars";
 import { Catalogue } from "@/sections/Catalogue";
 import { LogoTicker } from "@/sections/LogoTicker";
-import { Testimonials } from "@/sections/Testimonials";
 import { Location } from "@/sections/Location";
 import { CallToAction } from "@/sections/CallToAction";
 import { Footer } from "@/sections/Footer";
@@ -15,10 +13,8 @@ export default function Home() {
       <Header />
       <Carousel />
       <Gallery />
-      <CompanyPillars />
       <Catalogue />
       <LogoTicker />
-      <Testimonials />
       <Location />
       <CallToAction />
       <Footer />
