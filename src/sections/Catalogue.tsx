a18942cabe80d62b348b9eea4c9854a96f784b37
Aditya Ghadge <adityaghadge360@gmail.com>
2024-08-26 17:32:13 +0530

    align catalogue title to center

--- src/sections/Catalogue.tsx
+++ src/sections/Catalogue.tsx
@@ -2,7 +2,7 @@ export const Catalogue = () => {
   return (
     <section className="bg-catalogue w-full bg-no-repeat bg-cover h-[500px] flex flex-col items-center px-5 py-8 md:py-16 lg:py-20 gap-10">
       <div className="w-full border-b-8 border-white py-5 flex justify-center items-center md:w-1/2">
-        <h3 className="text-white text-3xl lg:text-4xl  font-extrabold">
+        <h3 className="text-white text-3xl lg:text-4xl  font-extrabold text-center">
           Browse Our Catalogue
         </h3>
       </div>
